62c3cfb430b67f283cd9697b1ec7b9857ef2acaf
Billie Thompson <billie@billiecodes.com>
2020-06-30 10:06:06 +0200

    Stuff

--- fish/config.fish
+++ fish/config.fish
@@ -53,23 +53,24 @@ if [ -f "$HOME/.config/envsecret/envsecret.sourceable.fish" ]
     set OLD_IFS "$IFS"
     set IFS ""
 
-    if set --query GOOGLE_APPLICATION_CREDENTIALS
-      set --local GOOGLE_APPLICATION_CREDENTIALS_OLD "$GOOGLE_APPLICATION_CREDENTIALS"
-      set GOOGLE_APPLICATION_CREDENTIALS_OLD "$GOOGLE_APPLICATION_CREDENTIALS"
+    if set --query CLOUDSDK_ACTIVE_CONFIG_NAME
+      set --local CLOUDSDK_ACTIVE_CONFIG_NAME_OLD "$CLOUDSDK_ACTIVE_CONFIG_NAME"
     end
 
-    set --global --export  GOOGLE_APPLICATION_CREDENTIALS "$XDG_CONFIG_HOME/gcloud/application_sops_credentials.json"
+    set --global --export  CLOUDSDK_ACTIVE_CONFIG_NAME "secret-store"
     set --local ENV_SECRETS (
       sops \
         exec-file \
         "$HOME/.config/envsecret/envsecret.sourceable.fish" \
         'cat "{}"'
     )
-    if set --query GOOGLE_APPLICATION_CREDENTIALS_OLD
-      set --global --export GOOGLE_APPLICATION_CREDENTIALS "$GOOGLE_APPLICATION_CREDENTIALS_OLD"
+    
+    if set --query  CLOUDSDK_ACTIVE_CONFIG_NAME_OLD
+      set --global --export  CLOUDSDK_ACTIVE_CONFIG_NAME "$CLOUDSDK_ACTIVE_CONFIG_NAME_OLD"
     else
-      set --erase GOOGLE_APPLICATION_CREDENTIALS
+      set --erase  CLOUDSDK_ACTIVE_CONFIG_NAME 
     end
+
 
     eval "$ENV_SECRETS"
     set IFS "$OLD_IFS"
