30b242caf6b9d377335d98c71d8351bbb8188ad3
Billie Thompson <billie@billiecodes.com>
2020-07-26 08:46:06 +0200

    Use mdcat for markdown ifles

--- fish/functions/cat.fish
+++ fish/functions/cat.fish
@@ -1,3 +1,7 @@
 function cat
-    bat -p --paging=never $argv
+    if test (count $argv) -eq 1 && string match --ignore-case --quiet "*.md" "$argv[1]"
+        mdcat $argv[1]
+    else
+        bat -p --paging=never $argv
+    end
 end
