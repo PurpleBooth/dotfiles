daacc04a230622049bb3499bc853f5a259bdcd65
Billie Thompson <billie@billiecodes.com>
2020-07-26 09:03:16 +0200

    Do images

--- fish/functions/cat.fish
+++ fish/functions/cat.fish
@@ -1,6 +1,17 @@
 function cat
-    if test (count $argv) -eq 1 && string match --ignore-case --quiet "*.md" "$argv[1]"
-        mdcat $argv[1]
+    if test (count $argv) -eq 1
+        switch (string lower -- "$argv[1]")
+            case '--version'
+                bat $argv
+            case '*.md'
+                mdcat $argv
+            case '*'
+                if identify $argv > /dev/null
+                  icat $argv
+                else
+                  bat -p --paging=never $argv
+                end
+        end
     else
         bat -p --paging=never $argv
     end
